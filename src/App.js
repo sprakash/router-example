fbd2dbbd1fb0c0461c952ccb35e1ca9d7acabf79
sprakash <prakashnitza@gmail.com>
2022-06-19 18:01:36 -0700

    redundant components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { Link } from "react-router-dom";
 import "./App.css";
-import About from "./components/About";
-import ClothingItem from "./components/ClothingItem";
-import Search from "./components/Search";
 
 function App() {
   return (
